0f4c537853fa2d201cfa84066dbf5c570d4ca9df
Julian Macias <al44728@un.edu.mx>
2024-06-13 17:52:38 -0600

    Primer Commit

---
+++ Entities/Jefe.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProyectoFinalPOO2.Entities
+{
+    public class Jefe
+    {
+         public Guid Id { get; set; }
+        public string NombreJefe { get; set; }
+        public string Departamento  { get; set; }
+
+        public int  Sueldo { get; set; }
+    }
+}
